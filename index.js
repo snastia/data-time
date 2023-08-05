63bc856c79aa6e9cf7f99224ba99a5509370f52b
Настя Скрицька <snastia.m@gmail.com>
2023-08-05 14:14:51 +0300

    class

---
+++ index.js
@@ -0,0 +1,54 @@
+const month = document.querySelector("h2")
+const text = document.querySelector("a")
+// const date = new Date()
+
+// month.innerHTML = `Month ${date.getMonth()}`
+
+// const date = new Date(2009, 10, 9)
+
+// console.log(date.getFullYear(), date.getMonth(), date.getDate())
+
+// console.log(date.getDate())
+// console.log(date.getDay())
+// console.log(date.getMonth())
+// console.log(date.getFullYear())
+// console.log(date.getMins())
+// console.log(date.getSeconds())
+// console.log(date.getTime())
+
+// const date = new Date()
+
+// function dateNow() {
+//     month.innerHTML = `Month: ${date.getMonth() + 1}
+//     day: ${date.getDate()} 
+//     hours: ${date.getHours()}:${date.getMinutes()}`
+// }
+
+// dateNow()
+
+// function year(date) {
+//     const dates = new Date(date)
+//     month.innerHTML = `${dates.getFullYear()}`
+    
+// }
+
+// year("2002-09-08")
+
+
+
+// function satSun() {
+//     const dates = new Date()
+
+//     let dayWeek = dates.getDay() === 0 || dates.getDay() === 6 ? `Weekend` : `Work day`
+
+//     month.innerHTML = `${dayWeek}`
+// }
+
+// satSun('2023-08-06')
+
+
+function days(day1, day2) {
+    const dates = new Date()
+
+    
+}
